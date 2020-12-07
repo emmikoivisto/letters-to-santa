fc988781bfc22946f410eeae14ca4ed70d231e9b
emmikoivisto <emetsi@gmail.com>
2020-12-07 14:11:19 +0000

    letterbox component added and renders

---
+++ src/components/LetterBox.js
@@ -0,0 +1,7 @@
+const LetterBox = () => {
+    return (
+        <h2>This is the letterbox component</h2>
+    );
+};
+
+export default LetterBox;
--- src/containers/LetterConsole.js
+++ src/containers/LetterConsole.js
@@ -1,10 +1,14 @@
 import {useState} from 'react';
+import LetterBox from '../components/LetterBox';
 
 
 //React component
 const LetterConsole = () => {
     return (
-        <h2>This is the letter console</h2>
+        <>
+            <h2>This is the letter console</h2>
+            <LetterBox />
+        </>
     )
 };
 
