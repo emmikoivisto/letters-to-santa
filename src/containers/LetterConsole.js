d96a0deadccfc57af97fe766deeb9ae010e79eee
emmikoivisto <emetsi@gmail.com>
2020-12-07 13:55:08 +0000

    letter console created

---
+++ src/containers/LetterConsole.js
@@ -0,0 +1,10 @@
+import {useState} from 'react';
+
+//React component
+const LetterConsole = () => {
+    return (
+        <h2>This is the letter console</h2>
+    )
+};
+
+export default LetterConsole;
