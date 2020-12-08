ec30252cff1b27e7071359dc306e30fb67aa19e2
emmikoivisto <emetsi@gmail.com>
2020-12-08 10:28:26 +0000

    onchange functions added to form

--- src/components/LetterForm.js
+++ src/components/LetterForm.js
@@ -8,17 +8,27 @@ const LetterForm = () => {
 
     const [text, setText] = useState(""); //text starts as an empty string
 
+    const handleAuthorChange = (event) => {
+        setAuthor(event.target.value)
+    };
+
+    const handleTextChange = (event) => {
+        setText(event.target.value)
+    };
+
     return (
         <form>
             <input 
                 type="text"
                 placeholder="Your Name"
                 value={author}
+                onChange={handleAuthorChange}  //onChange is a listener
             />
             <input
                 type="text"
                 placeholder="Say something.."
                 value={text}
+                onChange={handleTextChange}
             />
             <input
                 type="submit"
