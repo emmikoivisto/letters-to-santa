79273f41b7109fc00aa2e3cdcde09d8e44e4ddad
emmikoivisto <emetsi@gmail.com>
2020-12-08 10:08:07 +0000

    letter form added

---
+++ src/components/LetterForm.js
@@ -0,0 +1,18 @@
+const LetterForm = () => {
+    return (
+        <form>
+            <input 
+                type="text"
+                placeholder="Your Name"
+            />
+            <input
+                type="text"
+                placeholder="Say something.."
+            />
+            <input
+                type="submit"
+                value="Post"
+            />
+        </form>
+    )
+}
