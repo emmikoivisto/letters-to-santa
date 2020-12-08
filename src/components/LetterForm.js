fe101ef56d73aec6e04d3de89f27cb87af7c7a3c
emmikoivisto <emetsi@gmail.com>
2020-12-08 10:23:03 +0000

    state added to form

--- src/components/LetterForm.js
+++ src/components/LetterForm.js
@@ -1,13 +1,24 @@
+// forms always hold their own state
+
+import { useState } from "react";
+
 const LetterForm = () => {
+
+    const [author, setAuthor] = useState(""); //author starts as an empty string
+
+    const [text, setText] = useState(""); //text starts as an empty string
+
     return (
         <form>
             <input 
                 type="text"
                 placeholder="Your Name"
+                value={author}
             />
             <input
                 type="text"
                 placeholder="Say something.."
+                value={text}
             />
             <input
                 type="submit"
